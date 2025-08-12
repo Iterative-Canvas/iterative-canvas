e4603aab3b2703ae4c4d450cf2b670b7eb4718b8
John Munson <19480078+johncmunson@users.noreply.github.com>
2025-08-12 07:08:02 +0000

    init 2

---
+++ lib/envConfig.ts
@@ -0,0 +1,9 @@
+import { loadEnvConfig } from "@next/env"
+
+// Usage: import "./lib/envConfig.ts"
+// Necessary when running scripts in this repo that need to read environment variables but aren't
+// running as part of the NextJS runtime, which loads environment variables automatically.
+// https://nextjs.org/docs/app/guides/environment-variables#loading-environment-variables-with-nextenv
+
+const projectDir = process.cwd()
+loadEnvConfig(projectDir)
