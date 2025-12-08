f6605e45b5a1b14c07d8b6cc0a4ce19fe73b3f16
John Munson <19480078+johncmunson@users.noreply.github.com>
2025-12-08 21:31:56 +0000

    small changes

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,6 +13,15 @@ const compat = new FlatCompat({
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   eslintConfigPrettier,
+  {
+    ignores: [
+      "node_modules/**",
+      ".next/**",
+      "out/**",
+      "build/**",
+      "next-env.d.ts",
+    ],
+  },
 ]
 
 export default eslintConfig
